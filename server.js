86a79c279e8d5c95be729d52dea4aa3065fc0065
suraj-singh12 <surajsingh5092@gmail.com>
2022-05-20 18:57:07 +0530

    added help link in server

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ let app = express();
 
 let port = process.env.PORT || 4565;
 
+// read this guide 
+// https://www.positronx.io/how-to-serve-static-files-css-js-images-in-express-js/
+
 app.use(express.static('./'));
 app.use('/', express.static('./'));
 
